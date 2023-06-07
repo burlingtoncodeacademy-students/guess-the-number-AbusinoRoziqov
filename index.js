eb2c7c6b1ca29e24e5dc8919a836871a0ab958b2
Abusino Roziqov <jo2003king@gmail.com>
2023-06-07 15:54:29 -0600

    Done

--- index.js
+++ index.js
@@ -2,24 +2,14 @@ const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
 
 function ask(questionText) {
-  return new Promise((resolve, reject) => {
-    rl.question(questionText, resolve);
-  });
+return new Promise((resolve, reject) => {
+  rl.question(questionText, resolve);
+});
 }
-
 //  Global variables 
 
-
-
-
-
-
-
-
-
-
 function guessNumber(min, max) {
-  return Math.floor((min + max) / 2)
+return Math.floor((min + max) / 2)
 }
 // console.log (guessNumber (1, 100))
 
@@ -27,66 +17,67 @@ start();
 
 async function start() {
 
-  let minGuess = 1;
-  let maxGuess = 100;
+let minGuess = 1;
+let maxGuess = 100;
 
-
-
-
-
-
-  let startGame = await ask("Are you ready to play the game?")
-  console.log(startGame)
-  if ("Y" === startGame) {
-    console.log("User Said Yes!")
-
-    console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
-    let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-    let currentGuess = guessNumber(minGuess, maxGuess)
-    while (true) {
-      
+let startGame = await ask("Are you ready to play the game?")
+console.log(startGame)
+if ("Y" === startGame) {
+  console.log("User Said Yes!")
   
+  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
+  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
+  let currentGuess = guessNumber(minGuess, maxGuess)
+  while (true) {
+
+  console.log('You entered: ' + secretNumber);
+  console.log(`Is your number ${currentGuess}?`)
+  let yn = await ask(">")
+  console.log(yn)
+  if (yn === "N") {
+    let hl = await ask(`Is it Higher(H) or Lower(L)?`) 
+    if (currentGuess <= minGuess) {
+      console.log(`You cheated! You said it was lower than ${currentGuess}, so it can't also be higher than ${currentGuess - 1}!`);
+    }
     
-
-
-
-    console.log('You entered: ' + secretNumber);
-    console.log(`Is your number ${currentGuess}?`)
-    let yn = await ask(">")
-    console.log(yn)
-    if (yn === "N") {
-      let hl = await ask(`Is it Higher or Lower?`)
-      if (hl === "H") {
-        console.log("Hit H")
-        minGuess = currentGuess
-        console.log (`The guess range is now ${minGuess}, ${maxGuess}!`)
-        currentGuess = guessNumber (minGuess, maxGuess)
-        
-
-      } else if (hl === "L") {
-        console.log("Hit L")
-        maxGuess = currentGuess
-        console.log (`The guess range is now ${minGuess}, ${maxGuess}!`)
-        currentGuess = guessNumber (minGuess, maxGuess)
-
-      } else {
-        console.log("Please type H or L")
+    // Check if user guess is higher or lower then the current guess range 
+    // If the user says higher than max guess they are cheating and other way 
+    
+    if (hl === "H") {
+      console.log("Hit H")
+      minGuess = currentGuess
+      console.log (`The guess range is now ${minGuess}, ${maxGuess}!`)
+      currentGuess = guessNumber (minGuess, maxGuess)
+      if (currentGuess >= maxGuess) {
+        console.log(`You cheated! You said it was higher than ${currentGuess}, so it can't also be lower than ${currentGuess + 1}!`);
       }
 
-    } else if (yn === "Y") {
-      console.log(`Congratulations your number is ${currentGuess}!`)
-      process.exit();
+    } else if (hl === "L") {
+      console.log("Hit L")
+      maxGuess = currentGuess
+
+      console.log (`The guess range is now ${minGuess}, ${maxGuess}!`)
+      currentGuess = guessNumber (minGuess, maxGuess)
+
     } else {
-      console.log("Please enter Y or N")
+      console.log("Please type H or L")
     }
-  }
 
-
-
-  } else if ("N" === startGame) {
-    console.log("No")
+  } else if (yn === "Y") {
+    console.log(`Congratulations your number is ${currentGuess}!`)
+    process.exit();
   } else {
-    console.log("Type Y or N ")
+    console.log("Please enter Y or N")
   }
-  // Now try and complete the program.
 }
+
+
+
+} else if ("N" === startGame) {
+  console.log("No")
+} else {
+  console.log("Type Y or N ")
+}
+}
+
+// Now try and complete the program.
---
+++ reverse_game.js
@@ -0,0 +1,59 @@
+const readline = require('readline');
+
+// Function to generate a random number within a range
+function randomNumber(min, max) {
+return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Function to check if the user's guess is valid
+function isValidGuess(guess) {
+return Boolean(guess) && guess >= 1 && guess <= 100;
+}
+
+// Function to start the game
+function startGame() {
+const rl = readline.createInterface({
+input: process.stdin,
+output: process.stdout
+});
+console.log('The Number Guessing Game is here; welcome! where I (the computer) would generate a number and you (the human) would attempt to guess it ');
+askGuess();
+
+const secretNumber = randomNumber(1, 100);
+let attempts = 0;
+
+// Function where the game actually starts starting with asking questions
+function askGuess() {
+rl.question('Guess a number between 1 and 100: ', (guess) => {
+guess = parseInt(guess);
+
+if (!isValidGuess(guess)) {
+    console.log('Invalid guess. Please enter a number between 1 and 100.');
+    askGuess();
+    return;
+}
+
+attempts++;
+
+if (guess === secretNumber) {
+    console.log(`Congratulations! You guessed the secret number ${secretNumber} in ${attempts} attempts.`);
+    rl.question('Play it again? (yes/no): ', (answer) => {
+        if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+            startGame(); // Restart the game
+        } else {
+            rl.close(); // Exit the program
+        }
+    });
+} else if (guess < secretNumber) {
+    console.log('Too low! Try guessing higher.');
+    askGuess();
+} else {
+    console.log('Too high! Try guessing lower.');
+    askGuess();
+}
+});
+}
+}
+
+// Start the game
+startGame();
